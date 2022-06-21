1c4b68aa3e46b652d678b6bdadc07017e011c42b
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-06-21 22:54:33 +0100

    transaction receipt

--- deploy.js
+++ deploy.js
@@ -1,16 +1,23 @@
 const ethers = require('ethers')
 const fs = require('fs-extra')
+// console.log(fs)
 async function main() {
-    const provider = new ethers.providers.JsonRpcSigner('HTTP://127.0.0.1:7545')
+    const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545')
 
-    const wallet = new ethers.wallet('a3726b28e71450bb56cf8f48d88e8e0123fa48639c4a222df6aa11f7804657f6',provider);
-    const abi = fs.readFileSync('./simpleStorage_sol_SimpleStorage.abi','utf8')
-    const binary = fs.readFileSync('./simpleStorage_sol_SimpleStorage.bin','utf8')
+    const wallet = new ethers.Wallet('5679d9b63ef9cf2f799bac86b87520b9cfdd37422c599a50d9814e7a06dccbbf',provider);
+    const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi','utf8')
+    const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin','utf8')
+    const contractFactory = new ethers.ContractFactory(abi,binary,wallet)
+    console.log('deploying')
+    const contract = await contractFactory.deploy() // contract deployment
+    const transactionReciept = await contract.deployTransaction.wait(1) // transaction receipt
+    console.log(transactionReciept)
 }
 
-main().then(()=>{
-    process.exit(0)
-}).catch((error)=>{
-    process.exit(1)
-console.log(error)
-})
+main()
+ .then(()=> process.exit(0))
+ .catch((error)=> {
+     console.log(error)
+ }
+// console.log(error)
+)
