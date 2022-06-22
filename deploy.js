ee84837125b6456ff37a3d733d7497ab6e694809
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-06-22 10:44:57 +0100

    use dotenv package for private key

--- deploy.js
+++ deploy.js
@@ -1,10 +1,11 @@
 const ethers = require('ethers')
 const fs = require('fs-extra')
+require('dotenv').config()
 // console.log(fs)
 async function main() {
     const provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:7545')
 
-    const wallet = new ethers.Wallet('6137d809c35c8159ad8bfd2a61b958e0aa3adac6d634ad31c047d4d8e5134839',provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY,provider);
     const abi = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.abi','utf8')
     const binary = fs.readFileSync('./SimpleStorage_sol_SimpleStorage.bin','utf8')
     const contractFactory = new ethers.ContractFactory(abi,binary,wallet)
