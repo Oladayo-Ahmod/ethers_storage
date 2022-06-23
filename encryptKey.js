6a227470e43376fd6d70bb1e33bd7f072a84d4e5
oladayo-ahmod <oladayoahmod112@gmail.com>
2022-06-23 19:07:10 +0100

    encrypting private key

---
+++ encryptKey.js
@@ -0,0 +1,14 @@
+const ethers = require("ethers")
+const dotenv = require("dotenv")
+
+async function main(){
+    
+}
+
+main()
+ .then(()=> process.exit(0))
+ .catch((error)=> {
+     console.log(error)
+ }
+// console.log(error)
+)
